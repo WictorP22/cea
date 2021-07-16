51cd153889ee5954ec46d486941c15b83f9af080
WictorP22 <83567799+WictorP22@users.noreply.github.com>
2021-07-16 00:35:17 -0300

    Create main2.js

---
+++ js/main2.js
@@ -0,0 +1,85 @@
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Focus input ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+  
+  
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    var temporiza;
+    $(".input100").on("input", function() {
+        clearTimeout(temporiza);
+        temporiza = setTimeout(function() {
+            avatar();
+        }, 2000);
+    });
+
+    function avatar() {
+        var url1 = 'http://www.habbo.com.br/habbo-imaging/avatarimage?&user=';
+        var url2 = '&action=std&direction=2&head_direction=2&img_format=png&gesture=std&headonly=0&size=l';
+        var nome = $('.input100').val();
+        var junta = url1+nome+url2;
+        $('#avat').attr("src",junta);
+    }
+    
+})(jQuery);
